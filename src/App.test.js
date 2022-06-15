81ad1d62ca2e119d847f2050f0f8344728d2f167
florencia <flociccione@gmail.com>
2022-06-15 09:42:25 -0300

    weather app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders placeholder', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const placeholderText = screen.getByPlaceholderText("Enter the city whose weather you want to know...");
+  expect(placeholderText).toBeInTheDocument();
 });
